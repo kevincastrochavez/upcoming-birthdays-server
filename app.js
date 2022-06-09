c6e2320d070ca72a28d86c082dfcb6276fadba2e
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-09 11:17:03 -0600

    added Auth0 authentication to friends and users routes to be able to access them

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
 const swaggerDocument = require('./swagger-output.json');
 const friendsRoutes = require('./routes/friends');
 const usersRoutes = require('./routes/users');
 const mongodb = require('./db/connect');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
 app.use(cors());
+app.use(auth(config));
 
 app
   .use(bodyParser.json())
@@ -20,9 +32,14 @@ app
 
     next();
   })
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-  .use('/friends', friendsRoutes)
-  .use('/users', usersRoutes);
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.use('/friends', requiresAuth(), friendsRoutes);
+app.use('/users', requiresAuth(), usersRoutes);
 
 mongodb.initDb((err) => {
   if (err) {
