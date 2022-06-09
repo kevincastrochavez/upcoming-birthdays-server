828e465953d497d9a2d947945ca7a5d420b195b7
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-09 17:54:23 -0600

    moved user route for better organization

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ const usersController = require('../controllers/users');
 const validator = require('../middleware/validate');
 
 router.get('/', usersController.getAllUsers);
-router.post('/', validator.saveUser, usersController.createUser);
 router.get('/:id', validator.saveUser, usersController.getUser);
+router.post('/', validator.saveUser, usersController.createUser);
 router.put('/:id', usersController.updateUser);
 router.delete('/:id', usersController.deleteUser);
 
