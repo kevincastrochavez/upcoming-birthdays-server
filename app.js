4a86b2c3fbad8255eac6bd6934babd2784c7aa3f
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-10 13:28:20 -0600

    dumb change

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ mongodb.initDb((err) => {
     console.log(err);
   } else {
     app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
+    console.log(`Connected to DB and listening on PORT:${port}`);
   }
 });
