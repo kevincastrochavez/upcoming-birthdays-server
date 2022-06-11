6b51fc4532e386fbe9bd52c2abce6abab3ce9fe9
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-11 17:42:46 -0600

    removed validation from the route for getting users by id and put it in the correct one

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,9 @@ const usersController = require('../controllers/users');
 const validator = require('../middleware/validate');
 
 router.get('/', usersController.getAllUsers);
-router.get('/:id', validator.saveUser, usersController.getUser);
+router.get('/:id', usersController.getUser);
 router.post('/', validator.saveUser, usersController.createUser);
-router.put('/:id', usersController.updateUser);
+router.put('/:id', validator.saveUser, usersController.updateUser);
 router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
